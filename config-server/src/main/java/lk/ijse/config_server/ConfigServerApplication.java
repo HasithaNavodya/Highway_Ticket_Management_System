230024ecbe20f679f2363f0778189f5aa71a371b
Hasitha PC <hasithanavodya@gmail.com>
2024-07-03 14:32:52 +0530

    Modified Config Server

--- config-server/src/main/java/lk/ijse/config_server/ConfigServerApplication.java
+++ config-server/src/main/java/lk/ijse/config_server/ConfigServerApplication.java
@@ -2,10 +2,12 @@ package lk.ijse.config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
 public class ConfigServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
