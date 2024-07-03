23851d1f2551f76f3dcc99bbc0844e333ec2194e
Hasitha PC <hasithanavodya@gmail.com>
2024-07-03 14:41:38 +0530

    Modified Ticket Service

--- ticket-service/src/main/java/lk/ijse/ticket_service/TicketServiceApplication.java
+++ ticket-service/src/main/java/lk/ijse/ticket_service/TicketServiceApplication.java
@@ -3,6 +3,7 @@ package lk.ijse.ticket_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,6 +15,7 @@ public class TicketServiceApplication {
 	}
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
--- ticket-service/src/main/java/lk/ijse/ticket_service/controller/TicketController.java
+++ ticket-service/src/main/java/lk/ijse/ticket_service/controller/TicketController.java
@@ -4,15 +4,60 @@ package lk.ijse.ticket_service.controller;/*
     Date : 6/26/2024
 */
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.ticket_service.entity.Ticket;
+import lk.ijse.ticket_service.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
-@RequestMapping("/ticket")
+@RequestMapping("/api/v1/ticket")
 public class TicketController {
+    TicketController() {
+        System.out.println("vehicle invoked");
+    }
+
+//    @GetMapping
+//    public String get(){
+//        return "Ticket get method invoked";
+//    }
+
+    @Autowired
+    private TicketService ticketService;
+
+    @PostMapping
+    public boolean registerTicket() {
+        System.out.println("ticker invoke");
+//        try {
+//            Ticket savedTicket = ticketService.saveTicket(ticket);
+//            return new ResponseEntity<>(savedTicket, HttpStatus.CREATED);
+//        } catch (Exception e) {
+//            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+        return true;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Ticket> getVehicle(@PathVariable Long id) {
+        return ResponseEntity.ok(ticketService.getTicket(id));
+    }
+
     @GetMapping
-    public String get(){
-        return "Ticket get method invoked";
+    public ResponseEntity<List<Ticket>> getAllVehicles() {
+        return ResponseEntity.ok(ticketService.getAllTicket());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Ticket> updateVehicle(@PathVariable Long id, @RequestBody Ticket vehicle) {
+        return ResponseEntity.ok(ticketService.updateTicket(id, vehicle));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteVehicle(@PathVariable Long id) {
+        ticketService.deleteTicket(id);
+        return ResponseEntity.noContent().build();
     }
 }
